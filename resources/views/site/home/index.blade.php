98724336c823315334d1eabba4feacca1ae56ea9
willianoma <willianoma@hotmail.com>
2017-05-16 12:40:00 -0300

    sei la..

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -1,1 +1,32 @@
 Home do site na VIEW
+<html>
+<head>
+    <title>Mascara Telefone</title>
+    <script type="text/javascript" src="mtel.js"></script>
+
+</head>
+<body>
+
+<input type="text" name="telefone" onkeyup="mascara( this, mtel );" maxlength="15"/>
+
+</body>
+</html>
+
+<script>
+    /* Máscaras ER */
+
+    function mascara(o, f) {
+        v_obj = o
+        v_fun = f
+        setTimeout("execmascara()", 1)
+    }
+    function execmascara() {
+        v_obj.value = v_fun(v_obj.value)
+    }
+    function mtel(v) {
+        v = v.replace(/\D/g, "");             //Remove tudo o que não é dígito
+        v = v.replace(/^(\d{2})(\d)/g, "($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+        v = v.replace(/(\d)(\d{4})$/, "$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+        return v;
+    }
+</script>
